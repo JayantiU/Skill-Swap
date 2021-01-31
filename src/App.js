0843743596b4442cfb9c53a616acfe8e52ffb96d
Vivek Alamuri <vivek.alamuri@gmail.com>
2021-01-30 20:11:46 -0500

    screen captue in progress

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import RegisterPage from './components/RegisterPage/RegisterPage'
 import LoginPage from './components/LoginPage/LoginPage';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import ScreenCaptureComponent from './components/ScreenCaptureComponent.js/ScreenCaptureComponent';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
           <Switch>
             <Route path="/" exact component={LoginPage}/>
             <Route path="/register" component={RegisterPage}/>
+            <Route path="/scapture" component={ScreenCaptureComponent}/>
           </Switch>
       </div>
     </Router>
