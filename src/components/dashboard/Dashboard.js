368ced7f6fad65828319d034071ff52deebff3c3
Jayanti Upadhyay <updjayanti@gmail.com>
2021-01-30 20:48:47 -0500

    frontend tings

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Dashboard.css';
+import {Link} from 'react-router-dom'; 
+import dash from '../../img/dash.png';
+import Button from 'react-bootstrap/Button';
+
+
+function Dashboard() {
+
+  return (
+    <div class='landing-container'>
+        <Link to="/video">
+            <Button className="video_button"> 
+                Join Video
+            </Button>
+        </Link>
+      
+      
+      <Button className="pref_button">
+        Preferences
+      </Button>
+    </div>
+    
+  ); 
+}
+
+export default Dashboard;
