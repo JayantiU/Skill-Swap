657995cf910b2a9bd76e5027210608a6f665ffda
CheranMahalingam <65926174+CheranMahalingam@users.noreply.github.com>
2021-01-31 01:25:30 -0500

    Authentication to prevent exploitation of video conferencing

--- src/components/ConferencePage/ConferencePage.js
+++ src/components/ConferencePage/ConferencePage.js
@@ -1,9 +1,21 @@
 import React, { useEffect } from "react";
 import sad from "../../Sad";
+import firebase from "../../config";
+import { useHistory } from "react-router-dom";
 
 function ConferencePage() {
+  let history = useHistory();
+
   useEffect(() => {
-    sad();
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log(user);
+        sad();
+      } else {
+        history.push("/");
+        window.location.reload();
+      }
+    });
   }, []);
 
   return (
